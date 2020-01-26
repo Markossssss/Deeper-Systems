7cdc412ea3780d25a3d8f6d408866629bccbfe55
Markossssss <marcos.hr.pacheco@gmail.com>
2020-01-26 19:53:09 -0300

    Documentation fixed

--- interpreter.py
+++ interpreter.py
@@ -14,11 +14,11 @@ import json
 
 # Code
 
-# Opens de file and dumps its contents into a well formatted string
+# Opens the file and dumps it's contents into a well formatted string
 with open("source_file_2.json") as f:
     json_data = json.load(f)
 
-# Function for geting every project that a watcher is in
+# Function for getting every project that a watcher is in
 def watchers():
     # Makes a list with just the watchers
     watchers_list = list()
@@ -29,13 +29,13 @@ def watchers():
             watchers_list.append(watcher[j])
 
     dic_geral = {}
-    # Tests i wacther for every project
+    # Tests i watcher for every project
     for i in range(len(watchers_list)):
         projects_list = list()
         for j in range(len(json_data)):
             data_watcher = json_data[j]['watchers']
 
-            # If finds will add to list based on priority value
+            # If found will add to list based on priority value
             if watchers_list[i] in data_watcher:
                 item = list()
                 item.append(json_data[j]['name'])
@@ -70,7 +70,7 @@ def managers():
         for j in range(len(json_data)):
             data_manager = json_data[j]['managers']
 
-            # If finds will add to list based on priority value
+            # If found will add to list based on priority value
             if managers_list[i] in data_manager:
                 item = list()
                 item.append(json_data[j]['name'])
@@ -83,7 +83,7 @@ def managers():
             del projects_list[k][1]
             projects_list[k] = projects_list[k][0]
 
-        # Appends watcher i with list of projects hes in
+        # Appends manager i with list of projects hes in
         dic_geral.update({
             managers_list[i]:  projects_list
         })
@@ -94,7 +94,7 @@ result = watchers()
 with open('watchers.json', 'w') as file:
     json.dump(result, file, indent=2)
 
-# Writes file watchers.json
+# Writes file managers.json
 result = managers()
 with open('managers.json', 'w') as file:
     json.dump(result, file, indent=2)
